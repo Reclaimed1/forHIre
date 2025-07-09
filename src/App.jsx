5a83970d57222a66af6f162ce09aff32ddb4714e
Reclaimed1 <olanijunaid@gmail.com>
2025-07-09 20:01:11 +0100

    testing route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
 import './App.css'
 import Landing from './pages/landing/Landing';
+import Contact from './pages/contact/Contact';
 function App() {
 
   return (
@@ -8,6 +9,7 @@ function App() {
     <Router>
       <Routes>
         <Route path='/' element={<Landing/>}/>
+        <Route path='/contact' element={<Contact/>}/>
       </Routes>
     </Router>
      
