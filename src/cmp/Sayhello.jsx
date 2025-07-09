bf4872fadc97905874b638b8b0100e8f66b4e19b
Reclaimed1 <olanijunaid@gmail.com>
2025-07-09 20:32:55 +0100

    acube

--- src/cmp/Sayhello.jsx
+++ src/cmp/Sayhello.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 export default function Sayhello(){
     return (
         <div >
-            <div className="bg-[url(beffooter.png)] mt-18 bg-cover bg-center h-[40vh] w-full relative rounded-t-xl md:h-[90vh] ">
+            <div className="imagine4 mt-18 bg-cover bg-center h-[40vh] w-full relative rounded-t-xl md:h-[90vh] ">
              <div className="font-[Tektur] text-white px-3 py-3">
              <h1 className="text-4xl font-semibold">Say hello to brighter days together</h1>
              <p>indulge in the Ultimate Real Estate Experience</p>
